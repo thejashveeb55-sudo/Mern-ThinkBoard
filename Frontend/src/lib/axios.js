0155863bf962b2555b9908713d2534302102685f
Theju265 <thejashvee433@gmail.com>
2026-06-29 16:14:44 +0530

    Update API URL for deployment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://52.91.141.40:5002" : "/api";
+const BASE_URL = "http://52.91.141.40:5002";
 const api = axios.create({
   baseURL : BASE_URL,
 });
